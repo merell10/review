08ed32910e6b4d840cce550b2c8015a0facb7e7d
Ketlyn <merellketlyn1@gmail.com>
2021-09-04 11:54:57 -0300

    commit

--- src/main/java/com/projetoceos/review/controller/GamesController.java
+++ src/main/java/com/projetoceos/review/controller/GamesController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping
+@RequestMapping ("api")
 public class GamesController {
     
     @GetMapping("/games")
